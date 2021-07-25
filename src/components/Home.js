f94c8886567d02a6996a5dff9760a83382456ced
ahsan <ahsanmir28@gmail.com>
2021-07-25 23:29:59 +0500

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { Box, Typography, makeStyles } from "@material-ui/core";
+import { Box, makeStyles } from "@material-ui/core";
 import Youtube from "../Assets/Images/youtube.png";
 
 const useStyles = makeStyles({
   component: {
-    margin: 50,
+    margin: 0,
     "& > *": {
-      marginTop: 50,
+      marginRight: 0,
     },
   },
   image: {
-    width: "50%",
-    height: "50%",
+    width: "100%",
+    height: "700px",
   },
 });
 
@@ -19,7 +19,6 @@ const Home = () => {
   const classes = useStyles();
   return (
     <Box className={classes.component}>
-      <Typography variant="h4">Home Page</Typography>
       <Box style={{ display: "flex" }}>
         <img src={Youtube} className={classes.image} />
       </Box>
